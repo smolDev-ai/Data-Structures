45f2c1fb705c5eda8e086d2a4dd4b2ba30e0571b
Megan <megan.miller@smoldev.dev>
2020-02-24 15:00:58 -0600

    Stack done.

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,26 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
+# Because Stack is LIFO, all of the functions will invlove the head of the list.
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        if self.size != 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return
 
     def len(self):
-        pass
+        return self.size
